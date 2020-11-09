fa979f0112f6584104e181e6361ba06396400815
Van <vango85@gmail.com>
2020-11-09 15:43:29 +0100

    [xx] Post-lecture code

--- App.js
+++ App.js
@@ -1,26 +1,72 @@
-import React from 'react'
-import styled from 'styled-components/native'
-
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { Button, TouchableOpacity, Text } from 'react-native';
 const Container = styled.View`
   flex: 1;
-  background-color: papayawhip;
   justify-content: center;
   align-items: center;
-`
+`;
 
-const Title = styled.Text`
-  font-size: 24px;
-  color: palevioletred;
-`
+const TopContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  background-color: #222222;
+  justify-content: center;
+  align-items: center;
+`;
+const TopHeader = styled.Text`
+  font-size: 48px;
+  color: #efefef;
+`;
+
+const BottomContainer = styled.View`
+  flex: 3;
+  width: 100%;
+  background-color: #ffffff;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BottomText = styled.Text`
+  font-size: 32px;
+  color: #1f1f1f;
+`;
 
 const App = () => {
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        // Something is wrong
+        throw `Got some error: ${response.status}`;
+      }
+      return response.json();
+    })
+    .then((json) => {
+      // Do something cool with the data
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const [count, setCount] = useState(0);
+  const onIncrement = () => {
+    setCount(count + 1);
+    console.log('Incremented!');
+  };
+
   return (
     <Container>
-      <Title>This is your cool app!</Title>
-      <Title>Go to App.js and start coding</Title>
-      <Title>💅💅💅</Title>
+      <TopContainer>
+        <TopHeader>Counter</TopHeader>
+      </TopContainer>
+      <BottomContainer>
+        <TouchableOpacity onPress={onIncrement}>
+          <Text>Add +1</Text>
+        </TouchableOpacity>
+        <BottomText>Total: {count}</BottomText>
+      </BottomContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
