88e526f5eeae9de662c6c9e195e2a181087499ad
Van <vango85@gmail.com>
2020-11-10 23:32:08 +0100

    [xx] Post-lecture #2

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import HomeScreen from "./components/HomeScreen";
+import InfoScreen from "./components/InfoScreen";
+
+// Navigation Setup
+// https://reactnative.dev/docs/navigation
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <View></View>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Info" component={InfoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
-
 export default App;
